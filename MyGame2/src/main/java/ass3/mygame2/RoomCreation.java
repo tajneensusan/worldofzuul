e3e205bb501c45b4c0edb7df7fe8a873e3c3fcab
tajneensusan <88257862+tajneensusan@users.noreply.github.com>
2021-09-26 02:04:04 +0200

    Story 1 sprint 1
    Added new rooms to different direction

--- MyGame2/src/main/java/ass3/mygame2/RoomCreation.java
+++ MyGame2/src/main/java/ass3/mygame2/RoomCreation.java
@@ -26,16 +26,25 @@ public class RoomCreation {
     
     private void createRooms() {
 
-        Room castle, kitchen, frontGate;
+        Room castle, kitchen, frontGate, hallRoom, gallery, store;
 
         castle = new Room("castle", "You are at the castle", false);
         kitchen = new Room("kitchen", "The kitchen door has a shape of a heart", false);
         frontGate = new Room("frontGate", "There is a giant ogre", true);
+        hallRoom = new Room("hallRoom", "You are at the Hall room", false);
+        gallery = new Room("gallery", "You are at the Gallery", true);
+        store = new Room("store", "You are at the Store room", true);
         
 
         castle.setExit("east", kitchen);
         castle.setExit("south", frontGate);
         frontGate.setExit("north", castle);
+        kitchen.setExit("south", hallRoom);
+        hallRoom.setExit("north", kitchen);
+        hallRoom.setExit("east", gallery);
+        gallery.setExit("west", hallRoom);
+        gallery.setExit("south", store);
+        store.setExit("north", gallery);
 
         castle.addItemInRoom(itemCreation.getItem("excaliburSword"));
         castle.addItemInRoom(itemCreation.getItem("key"));
@@ -44,6 +53,9 @@ public class RoomCreation {
         allRoomInGame.add(castle);
         allRoomInGame.add(frontGate);
         allRoomInGame.add(kitchen);
+        allRoomInGame.add(hallRoom);
+        allRoomInGame.add(gallery);
+        allRoomInGame.add(store);
 
     }
 
